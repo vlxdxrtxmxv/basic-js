2f33e63a504a06c2a32bbf5bed577149f521c32b
vlxdxrtxmxv <103958051+vlxdxrtxmxv@users.noreply.github.com>
2022-10-15 22:38:26 +0300

    Update mine-sweeper.js

--- src/mine-sweeper.js
+++ src/mine-sweeper.js
@@ -23,9 +23,36 @@ const { NotImplementedError } = require('../extensions/index.js');
  *  [1, 1, 1]
  * ]
  */
-function minesweeper(/* matrix */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function minesweeper(matrix) {
+const res = [];
+for(let i = 0;i < matrix.length;i++)
+  {
+  const num = [];
+  for(let j =0; j< matrix[i].length; j++)
+    {
+      let count = 0;
+
+      if(matrix[i-1] !== undefined)
+      {
+        if(matrix[i-1][j-1] !=undefined && matrix[i-1][j-1]=== true) count++;
+        if(matrix[i-1][j] !=undefined && matrix[i-1][j]=== true) count++;
+        if(matrix[i-1][j+1] !=undefined && matrix[i-1][j+1]=== true) count++;
+      }
+
+      if(matrix[i][j-1] !== undefined && matrix[i][j-1]=== true) count++;
+      if(matrix[i][j+1] !== undefined && matrix[i][j+1]=== true) count++;
+
+      if(matrix[i+1] !== undefined)
+      {
+        if(matrix[i+1][j] !== undefined && matrix[i+1][j] === true) count++;
+        if(matrix[i+1][j-1] !== undefined && matrix[i+1][j-1] === true) count++;
+        if(matrix[i+1][j+1] !== undefined && matrix[i+1][j+1] === true) count++;
+      }
+      num.push(count);
+    }
+    res.push(num);
+  }
+  return res;
 }
 
 module.exports = {
