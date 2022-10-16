9df753d6e54c5289f3d5b1203d13c4d53d38f5d5
vlxdxrtxmxv <103958051+vlxdxrtxmxv@users.noreply.github.com>
2022-10-16 23:02:53 +0300

    Update vigenere-cipher.js

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -20,13 +20,66 @@ const { NotImplementedError } = require('../extensions/index.js');
  * 
  */
 class VigenereCipheringMachine {
-  encrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  constructor(value)
+  {
+    this.value = value;
   }
-  decrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  
+  encrypt(message, key) {
+   if(message === undefined || key === undefined || message === '')
+   {
+     throw new Error('Incorrect arguments!');
+   }
+  key = key.toUpperCase();
+  let newmes = message.toUpperCase();
+  let counter = 0;
+  let output = [];
+  for(let i = 0; i< newmes.length; i++)
+    {
+      if(counter === key.length)
+      {
+        counter = 0;
+      }
+      if(newmes.charCodeAt(i)> 64 && newmes.charCodeAt(i) < 91)
+      {
+        let tmp = (newmes.charCodeAt(i)+key.charCodeAt(counter))% 26;
+        output.push(alphabet[tmp]);
+        counter++;
+      }
+      else{
+        output.push(newmes[i]);
+      }
+    }
+    if(this.value === false)
+    {
+      output.reverse();
+    }
+    return output.join('');
+  }
+
+  decrypt(message , key) {
+    if (message == undefined || key == undefined || message === '') {
+      throw new Error('Incorrect arguments!');
+    }
+    key = key.toUpperCase();
+    let messtr = '';
+    messtr = message.toUpperCase();
+    let index = [];
+    let countstr = 0;
+    for(let i = 0; i < messtr.length; i++) {
+    if (countstr === key.length) {
+        countstr = 0;
+      }
+      if(messtr.charCodeAt(i) > 64 && messtr.charCodeAt(i) < 91) {
+        let tmp = ((messtr.charCodeAt(i) - key.charCodeAt(countstr)) + 26 ) % 26;
+        index.push(alphabet[tmp]);
+        countstr++;
+      } else {
+        index.push(messtr[i]);
+      }
+    }
+    if(this.ttype === false) index.reverse();
+    return index.join('');
   }
 }
 
