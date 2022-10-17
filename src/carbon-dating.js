1ca0a3b95e2a7603c0445d103072de4d446906aa
vlxdxrtxmxv <103958051+vlxdxrtxmxv@users.noreply.github.com>
2022-10-17 21:13:23 +0300

    Update carbon-dating.js

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -18,10 +18,7 @@ const HALF_LIFE_PERIOD = 5730;
  *
  */
 function dateSample(sampleActivity) {
-  if(sampleActivity === undefined || typeof sampleActivity !== 'string' || sampleActivity <= 0 || sampleActivity === MODERN_ACTIVITY || sampleActivity === NaN || isNaN(parseFloat(sampleActivity)) || parseFloat(sampleActivity) || isNaN(sampleActivity))
-  {
-    return false;
-  }
+ if ((typeof sampleActivity !== 'string') || Number.isNaN(+sampleActivity) || (+sampleActivity < 1) || (+sampleActivity > MODERN_ACTIVITY)) return false;
   sampleActivity = Number(sampleActivity);
 let hlp = 0.693/HALF_LIFE_PERIOD;
   let result =Math.ceil(Math.log((MODERN_ACTIVITY/sampleActivity))/hlp);
