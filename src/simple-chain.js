9f9ab4e96bb3171d3da1e84db1c915a6d39de620
vlxdxrtxmxv <103958051+vlxdxrtxmxv@users.noreply.github.com>
2022-10-17 21:34:53 +0300

    Update simple-chain.js

--- src/simple-chain.js
+++ src/simple-chain.js
@@ -5,37 +5,33 @@ const { NotImplementedError } = require('../extensions/index.js');
  * 
  */
 const chainMaker = {
-  chain : [],
-  getLength()
-    {
-      return this.chain.length;
-    },
-addLink(value){
-  value === undefined ? this.chain.push('()') : this.chain.push(`(${value})`);
-  return this;
-},
-  removeLink(position)
-    {
-      if (!this.chain[position] || !Number.isInteger(position) || position === 0) {
+  chain: [],
+  getLength() {
+    return this.chain.length;
+  },
+  addLink(value) {
+    value === undefined ? this.chain.push(`()`) : this.chain.push(`( ${value} )`);
+    return this
+
+  },
+  removeLink(position) {
+    if (!this.chain[position] || !Number.isInteger(position) || position === 0) {
       this.chain = []
       throw new Error('You can\'t remove incorrect link!');
     }
-      
-        this.chain.splice(position-1, 1);
-      
-      return this;
-    },
-  reverseChain(){
-    this.chain.reverse();
-    return this;
+    this.chain.splice(position - 1, 1)
+    return this
   },
-finishChain(){
-  let result = this.chain.join('~~');
-  this.chain = [];
-  return result;
-}
+  reverseChain() {
+    this.chain.reverse()
+    return this
+  },
+  finishChain() {
 
-  
+    let result = this.chain.join('~~');
+    this.chain = []
+    return result
+  }
 };
 
 module.exports = {
